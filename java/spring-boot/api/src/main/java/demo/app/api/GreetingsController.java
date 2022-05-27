823ca85f6fd7b73de05926363795de1469f73d10
Elio Bischof <eliobischof@gmail.com>
2022-05-27 15:37:42 +0200

    remove unused imports

--- java/spring-boot/api/src/main/java/demo/app/api/GreetingsController.java
+++ java/spring-boot/api/src/main/java/demo/app/api/GreetingsController.java
@@ -2,7 +2,6 @@ package demo.app.api;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- java/spring-boot/api/src/main/java/demo/app/config/WebSecurityConfig.java
+++ java/spring-boot/api/src/main/java/demo/app/config/WebSecurityConfig.java
@@ -4,13 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 /**
  * Configuration applied on all web endpoints defined for this
